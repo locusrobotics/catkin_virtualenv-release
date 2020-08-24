59c8cd40d02d01048f1a646f780993c811bf5df2
Paul Bovbel <paul@bovbel.com>
2020-08-24 17:13:29 -0400

    Rebase from 'debian/melodic/catkin_virtualenv'

--- src/catkin_virtualenv/__init__.py
+++ src/catkin_virtualenv/__init__.py
@@ -16,3 +16,29 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import logging.config
+import os
+import subprocess
+import yaml
+
+logger = logging.getLogger(__name__)
+
+
+def configure_logging():
+    try:
+        with open(os.environ["ROS_PYTHON_LOG_CONFIG_FILE"]) as config:
+            logging.config.dictConfig(yaml.safe_load(config))
+    except KeyError:
+        logging.basicConfig()
+
+    return logging.getLogger()
+
+
+def run_command(cmd, *args, **kwargs):
+    logger.info(" ".join(cmd))
+    if kwargs.pop("capture_output", False):
+        kwargs["stdout"] = subprocess.PIPE
+        kwargs["stderr"] = subprocess.PIPE
+    return subprocess.run(cmd, *args, **kwargs)
